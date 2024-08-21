89a8f31eca5ea3951e0325099b463d99f3455729
Arpan avvari <arpanavvari@Arpans-MacBook-Pro.local>
2024-08-21 14:32:26 -0400

    Fixed coverage check logic and removed unnecessary JS file

--- Comcast coding.py
+++ Comcast coding.py
@@ -17,8 +17,8 @@ output_1 = isCovered(ranges_1, left_1, right_1)
 
 # Example 2
 ranges_2 = [[1, 10], [10, 20]]
-left_2 = 21
-right_2 = 21
+left_2 = 22
+right_2 = 22
 output_2 = isCovered(ranges_2, left_2, right_2)
 
 print(output_1, output_2)
