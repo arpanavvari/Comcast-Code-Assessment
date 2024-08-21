70c8d4e59ba3fc0b9e2caa5555f87e534a9364d2
Arpan avvari <arpanavvari@Arpans-MacBook-Pro.local>
2024-08-21 15:40:41 -0400

    adding theChanges

--- Comcast coding.py
+++ Comcast coding.py
@@ -1,4 +1,4 @@
-def isCovered(ranges, left, right):
+def fullycovered(ranges, left, right):
     for i in range(left, right + 1):
         covered = False
         for start, end in ranges:
@@ -13,13 +13,15 @@ def isCovered(ranges, left, right):
 ranges_1 = [[1, 2], [3, 4], [5, 6]]
 left_1 = 2
 right_1 = 5
-output_1 = isCovered(ranges_1, left_1, right_1)
+output_1 = fullycovered(ranges_1, left_1, right_1)
 
 # Example 2
 ranges_2 = [[1, 10], [10, 20]]
-left_2 = 22
-right_2 = 22
-output_2 = isCovered(ranges_2, left_2, right_2)
+left_2 = 21
+right_2 = 21
+output_2 = fullycovered(ranges_2, left_2, right_2)
 
 print(output_1, output_2)
+
+
 
